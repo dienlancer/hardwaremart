3acd3f916bee5919eaa0f056320bb1f4bf98d5c3
trietnk01 <trietnk01@gmail.com>
2018-03-08 03:00:39 +0700

    cập nhật

--- resources/views/adminsystem/invoice/form.blade.php
+++ resources/views/adminsystem/invoice/form.blade.php
@@ -11,7 +11,7 @@ $inputEmail             =   '<input type="text" class="form-control" name="email
 $inputFullName          =   '<input type="text" class="form-control" name="fullname"                value="'.@$arrRowData['fullname'].'">';  
 $inputAddress           =   '<input type="text" class="form-control" name="address"                  value="'.@$arrRowData['address'].'">'; 
 $inputPhone             =   '<input type="text" class="form-control" name="phone"                      value="'.@$arrRowData['phone'].'">';  
-
+$inputNote              =   '<textarea  name="note" rows="2" cols="100" class="form-control" >'.@$arrRowData['note'].'</textarea>'; 
 
 $lblQuantity            =   number_format((int)@$arrRowData['quantity'],0,".",",");
 $lblTotalPrice          =   number_format((int)@$arrRowData['total_price'],0,".",",");
@@ -34,11 +34,11 @@ $product_height = $setting['product_height']['field_value'];
             <span class="caption-subject font-dark sbold uppercase">{{$title}}</span>
         </div>
         <div class="actions">
-         <div class="table-toolbar">
+           <div class="table-toolbar">
             <div class="row">
                 <div class="col-md-12">
                     <button onclick="save()" class="btn purple">Save new <i class="fa fa-floppy-o"></i></button> 
-                    <a href="<?php echo $linkCancel; ?>" class="btn green">Cancel <i class="fa fa-ban"></i></a>                    </div>                                                
+                    <a href="<?php echo $linkCancel; ?>" class="btn green">Cancel <i class="fa fa-ban"></i></a></div>                                                
                 </div>
             </div>    
         </div>
@@ -81,69 +81,80 @@ $product_height = $setting['product_height']['field_value'];
                     </div>      
                 </div>       
                 <div class="row">
-                   <div class="form-group col-md-6">
-                    <label class="col-md-3 control-label"><b>Mã đơn hàng</b></label>
-                    <div class="col-md-9">
-                        <?php echo $inputCode; ?>
-                        <span class="help-block"></span>
-                    </div>
+                    <div class="form-group col-md-6">
+                        <label class="col-md-3 control-label"><b>Mã đơn hàng</b></label>
+                        <div class="col-md-9">
+                            <?php echo $inputCode; ?>
+                            <span class="help-block"></span>
+                        </div>
+                    </div>   
+                    <div class="form-group col-md-6">
+                        <label class="col-md-3 control-label"><b>Trạng thái giao hàng</b></label>
+                        <div class="col-md-9">                            
+                            <?php echo $ddlStatus; ?>
+                            <span class="help-block"></span>
+                        </div>
+                    </div>     
                 </div>   
-                <div class="form-group col-md-6">
-                    <label class="col-md-3 control-label"><b>Trạng thái giao hàng</b></label>
-                    <div class="col-md-9">                            
-                        <?php echo $ddlStatus; ?>
-                        <span class="help-block"></span>
-                    </div>
-                </div>     
-            </div>                                             
-            <div class="row">
-                <div class="col-md-12">
-                    <table width="100%" class="com_product16">
-                        <thead>
-                            <tr>
-                                <th align="center"><center>Mã sản phẩm</center></th>
-                                <th align="center"><center>Tên sản phẩm</center></th>
-                                <th align="center"><center>Hình</center></th>
-                                <th align="center"><center>Giá</center></th>
-                                <th align="center"><center>Số lượng</center></th>
-                                <th align="center"><center>Thành tiền</center></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            foreach ($arrInvoiceDetail as $key => $value) {
-                                $product_code=$value["product_code"];
-                                $product_name=$value["product_name"];
-                                $product_image=asset('/upload/'.$product_width . 'x'.$product_height.'-'.$value["product_image"]) ;                
-                                $product_price=fnPrice($value["product_price"]);
-                                $product_quantity=$value["product_quantity"];
-                                $product_total_price=fnPrice($value["product_total_price"]);
-                                ?>
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label class="col-md-3 control-label"><b>Ghi chú</b></label>
+                        <div class="col-md-9">                            
+                            <?php echo $inputNote; ?>
+                            <span class="help-block"></span>
+                        </div>
+                    </div>   
+                    <div class="form-group col-md-6">
+                        
+                    </div>     
+                </div>                                                
+                <div class="row">
+                    <div class="col-md-12">
+                        <table width="100%" class="com_product16">
+                            <thead>
                                 <tr>
-                                    <td align="center"><?php echo $product_code; ?></td>
-                                    <td><?php echo $product_name; ?></td>
-                                    <td width="10%" align="center"><img style="width:100%" src="<?php echo $product_image; ?>" /></td>
-                                    <td align="right"><?php echo $product_price; ?></td>
-                                    <td width="10%" align="center"><b><center><?php echo $product_quantity; ?></center></b> </td>
-                                    <td width="15%" align="right"><b><?php echo $product_total_price; ?></b></td>
+                                    <th align="center"><center>Mã sản phẩm</center></th>
+                                    <th align="center"><center>Tên sản phẩm</center></th>
+                                    <th align="center"><center>Hình</center></th>
+                                    <th align="center"><center>Giá</center></th>
+                                    <th align="center"><center>Số lượng</center></th>
+                                    <th align="center"><center>Thành tiền</center></th>
                                 </tr>
+                            </thead>
+                            <tbody>
                                 <?php
-                            } 
-                            ?>              
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td colspan="4" align="center"><b>Tổng cộng</b></td>
-                                <td align="center"><b><?php echo (@$arrRowData["quantity"]); ?></b></td>
-                                <td align="right"><b><?php echo fnPrice(@$arrRowData["total_price"]); ?></b></td>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
-            </div>                                                                    
-        </div>                         
-    </form>
-        
+                                foreach ($arrInvoiceDetail as $key => $value) {
+                                    $product_code=$value["product_code"];
+                                    $product_name=$value["product_name"];
+                                    $product_image=asset('/upload/'.$product_width . 'x'.$product_height.'-'.$value["product_image"]) ;                
+                                    $product_price=fnPrice($value["product_price"]);
+                                    $product_quantity=$value["product_quantity"];
+                                    $product_total_price=fnPrice($value["product_total_price"]);
+                                    ?>
+                                    <tr>
+                                        <td align="center"><?php echo $product_code; ?></td>
+                                        <td><?php echo $product_name; ?></td>
+                                        <td width="10%" align="center"><img style="width:100%" src="<?php echo $product_image; ?>" /></td>
+                                        <td align="right"><?php echo $product_price; ?></td>
+                                        <td width="10%" align="center"><b><center><?php echo $product_quantity; ?></center></b> </td>
+                                        <td width="15%" align="right"><b><?php echo $product_total_price; ?></b></td>
+                                    </tr>
+                                    <?php
+                                } 
+                                ?>              
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td colspan="4" align="center"><b>Tổng cộng</b></td>
+                                    <td align="center"><b><?php echo (@$arrRowData["quantity"]); ?></b></td>
+                                    <td align="right"><b><?php echo fnPrice(@$arrRowData["total_price"]); ?></b></td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>                                                                    
+            </div>                         
+        </form>
     </div>
 </div>
 <script type="text/javascript" language="javascript">
@@ -160,16 +171,16 @@ $product_height = $setting['product_height']['field_value'];
         $(fullname).closest('.form-group').removeClass("has-error");
         $(address).closest('.form-group').removeClass("has-error");        
         $(phone).closest('.form-group').removeClass("has-error");
-     
-     
-            
+
+
+
         $(status).closest('.form-group').removeClass("has-error");        
         
         $(fullname).closest('.form-group').find('span').empty().hide();
         $(address).closest('.form-group').find('span').empty().hide();        
         $(phone).closest('.form-group').find('span').empty().hide();
-      
-     
+
+
         
         $(status).closest('.form-group').find('span').empty().hide();        
     }
@@ -178,10 +189,7 @@ $product_height = $setting['product_height']['field_value'];
         var id=$('input[name="id"]').val();                
         var fullname=$('input[name="fullname"]').val();
         var address=$('input[name="address"]').val();
-        var phone=$('input[name="phone"]').val();
-
-  
-        
+        var phone=$('input[name="phone"]').val();                
         var status=$('select[name="status"]').val();     
         var token = $('input[name="_token"]').val();   
         resetErrorStatus();
@@ -191,7 +199,7 @@ $product_height = $setting['product_height']['field_value'];
             "address":address,
             "phone":phone,
 
-  
+
             
             "status":status,
             "_token": token
